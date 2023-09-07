8c70a87f4f9367241bfb72fa2ec57f0afed3630e
ErickMwirigi <erickmbae21@gmail.com>
2023-09-07 16:27:56 +0300

    Done Lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,22 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+   
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/directors" component={Directors} />
+          <Route path="/actors" component={Actors} />
+        </Switch>
+      </div>
+    
+  );
 }
 
 export default App;
